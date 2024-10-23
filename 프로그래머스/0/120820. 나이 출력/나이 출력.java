ba60ab4d8a2a869f537db26783d621a3fab83fc0
ExProcurement <160109697+ExProcurement@users.noreply.github.com>
2024-10-23 19:08:08 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int bornYear = 0;
+        
+        bornYear = (2022-age)+1;
+        
+        answer = bornYear;
+        
+        return answer;
+    }
+}
