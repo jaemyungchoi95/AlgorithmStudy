3e1c30bf3d1f69dc3355706cc96917acc582c7db
ExProcurement <160109697+ExProcurement@users.noreply.github.com>
2024-10-30 19:46:00 +0900

    [level 0] Title: l로 만들기, Time: 25.73 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = []
+    
+    for i in myString:
+        if ord(i) < ord('l'):
+            answer.append('l')
+        else:
+            answer.append(i)
+    
+    return ''.join(answer)
