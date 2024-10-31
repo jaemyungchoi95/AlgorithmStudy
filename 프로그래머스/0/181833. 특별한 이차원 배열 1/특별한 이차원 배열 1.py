0a5670a7860d980e1ab38143379abe50ca7f24dd
ExProcurement <160109697+ExProcurement@users.noreply.github.com>
2024-10-31 20:58:15 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.09 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,10 +1,9 @@
 def solution(n):
     answer = []
-    temp = [0] * n
     
     for i in range(n):
+        temp = [0] * n
         temp[i] += 1
         answer.append(temp)
-        temp = [0] * n
     
     return answer
