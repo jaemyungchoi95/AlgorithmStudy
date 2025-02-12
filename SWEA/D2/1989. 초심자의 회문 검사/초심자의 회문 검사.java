89211e1ab993dab59b778b6c1c5b90668ebf0356
jaemyungchoi95 <160109697+jaemyungchoi95@users.noreply.github.com>
2025-02-12 13:46:49 +0900

    [D2] Title: 초심자의 회문 검사, Time: 101 ms, Memory: 26,368 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            
+			String str = sc.next(); // 회문 대상 문자열
+			int N = str.length(); // 문자열의 길이
+			StringBuilder sb = new StringBuilder();
+			
+			// 문자열을 역순으로 돌면서 sb에 담아준다
+			for (int i = N-1; i >= 0; i--) {
+				sb.append(str.charAt(i));
+			}
+			
+			// 만약 str과 sb가 일치한다면 1을 반환하고 아니면 0을 반환한다
+			if (str.equals(sb.toString())) {
+				System.out.printf("#%d %d\n", test_case, 1);
+			} else {
+				System.out.printf("#%d %d\n", test_case, 0);
+			}
+
+		}
+	}
+}
