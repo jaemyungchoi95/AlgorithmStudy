531eac2c8aad41f947b92d407da54937a6f77be5
jaemyungchoi95 <160109697+jaemyungchoi95@users.noreply.github.com>
2025-03-02 18:21:32 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 123 ms, Memory: 28,416 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,75 +1,45 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-class Solution
-{
-    static ArrayList<String> stack = new ArrayList<>(); // ArrayList로 스택을 구현한다
-    
-	public static void main(String args[]) throws Exception
-	{
+public class Solution {
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
-        
-        // 이 문제의 테스트케이스는 10개
-		for(int test_case = 1; test_case <= 10; test_case++) {	
-			stack.clear();
-			int T = sc.nextInt(); // 테스트 문자의 길이
-			sc.nextLine(); // 개행 문자 처리
-			String[] arr = sc.nextLine().split("");
-			// 한글자씩 받아진 배열을 탐색하면서
-			for (String str : arr) {
-				// 스택이 비었으면 무조건 추가
-				if (isEmpty()) {
-					push(str);
-					// 만약 직전의 괄호가 지금 탐색하는 괄호와 동일하다면 각 경우별로 직전의 값을 제거해준다
+		
+		// 이 문제에는 총 10개의 test_case가 주어진다
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			int N = sc.nextInt();
+			sc.nextLine(); // 개행문자 제거
+			String[] s = sc.nextLine().split("");
+			
+			Deque<String> stack = new ArrayDeque<>();
+			
+			for (String str : s) {
+				if (stack.isEmpty()) {
+					stack.addFirst(str);
 				} else {
-					if (str.equals(")") && peek().equals("(")) {
-						pop();
-					} else if (str.equals("}") && peek().equals("{")) {
-						pop();
-					} else if (str.equals("]") && peek().equals("[")) {
-						pop();
-					} else if (str.equals(">") && peek().equals("<")) {
-						pop();
+					if (str.equals(")") && stack.peekFirst().equals("(")) {
+						stack.removeFirst();
+					} else if (str.equals("}") && stack.peekFirst().equals("{")) {
+						stack.removeFirst();
+					} else if (str.equals("]") && stack.peekFirst().equals("[")) {
+						stack.removeFirst();
+					} else if (str.equals(">") && stack.peekFirst().equals("<")) {
+						stack.removeFirst();
 					} else {
-						push(str);
+						stack.addFirst(str);
 					}
 				}
 			}
+			
 			// 배열의 탐색이 끝났을 때 빈배열이라면 정답 출력
-			if (isEmpty()) {
+			if (stack.isEmpty()) {
 				System.out.printf("#%d %d\n", test_case, 1);
 			} else {
 				System.out.printf("#%d %d\n", test_case, 0);
 			}
-		}
-        sc.close();
-	}
-    
-	// 추가
-	static void push(String value) {
-		stack.add(value);
-	}
-	
-	// 확인
-	static String peek() {
-//		System.out.printf("peek : %s\n", stack.get(stack.size()-1));
-		return stack.get(stack.size()-1);
-	}
-	
-	// 삭제
-	static String pop() {
-//		System.out.printf("pop : %s\n", stack.get(stack.size()-1));
-		if (stack.isEmpty()) return "";
-		return stack.remove(stack.size()-1);
-	}
-	
-	// 빈 배열 확인
-	static boolean isEmpty() {
-		if(stack.size() == 0) {
-			return true;
-		} else {
-			return false;
+			
 		}
 	}
 }
