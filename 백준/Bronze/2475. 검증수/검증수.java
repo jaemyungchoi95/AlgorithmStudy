21d8e87940f615dd82f0651b627e528278f9ae80
jaemyungchoi95 <160109697+jaemyungchoi95@users.noreply.github.com>
2025-03-06 22:19:11 +0900

    [Bronze V] Title: 검증수, Time: 116 ms, Memory: 14536 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int sum = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			int tmp = Integer.parseInt(st.nextToken());
+			sum += tmp * tmp;
+		}
+		// 정답 출력 
+		bw.write(sum%10 + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
