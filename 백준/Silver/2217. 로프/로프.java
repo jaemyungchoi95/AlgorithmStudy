da67727a4ea3de0fff03317bcea620a5354635ca
jaemyungchoi95 <160109697+jaemyungchoi95@users.noreply.github.com>
2025-04-10 15:29:27 +0900

    [Silver IV] Title: 로프, Time: 996 ms, Memory: 111904 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,31 @@
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+	//로프
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N =sc.nextInt();
+		
+		List<Integer> list  = new ArrayList<>();
+		
+		for(int i =0 ; i<N;i++) {
+			list.add(sc.nextInt());
+		}
+		
+		Collections.sort(list);
+		int ans =0;
+		
+		for(int i =0;i<N;i++) {
+			int tmp =list.get(i)*(N-i);
+			ans=Math.max(ans, tmp);
+		}
+		System.out.println(ans);
+	}
+
+}
