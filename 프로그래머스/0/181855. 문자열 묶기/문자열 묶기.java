5f71f18b71d6e77739813e59b5f9f9ecaf45dd76
jaemyungchoi95 <160109697+jaemyungchoi95@users.noreply.github.com>
2025-08-26 16:50:21 +0900

    [level 0] Title: 문자열 묶기, Time: 8.80 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        int[] answer = new int[31];
+        
+        for (int i = 0; i < strArr.length; i++) {
+            answer[strArr[i].length()]++;
+        }
+        
+        return Arrays.stream(answer).max().getAsInt();
+    }
+}
